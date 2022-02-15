1412a7067273d2bd6d63288277b79f7b533e00d5
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-15 17:45:42 +0100

    Fetching Genres and rendering view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,8 +1,14 @@
 const Genre = require("../models/genre");
 
 // Display list of all Genre.
-exports.genre_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+exports.genre_list = (req, res, next) => {
+  Genre.find()
+    .sort([["name", "ascending"]])
+    .exec((err, list_genre) => {
+      if (err) return next(err);
+      //If successfull, render data in view
+      res.render("genre_list", { title: "Genre List", genre_list: list_genre });
+    });
 };
 
 // Display detail page for a specific Genre.
