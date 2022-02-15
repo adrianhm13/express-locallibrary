b4503052565fac08668c776d0f62fa3937442acd
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-15 18:31:13 +0100

    Added genre details view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 const Genre = require("../models/genre");
+const Book = require("../models/book");
+
+const async = require("async");
 
 // Display list of all Genre.
 exports.genre_list = (req, res, next) => {
@@ -12,8 +15,25 @@ exports.genre_list = (req, res, next) => {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre detail: " + req.params.id);
+exports.genre_detail = (req, res, next) => {
+  async.parallel({
+    genre: function(callback) {
+      Genre.findById(req.params.id).exec(callback);
+    },
+    genre_books: function(callback) {
+      Book.find({ 'genre': req.params.id }).exec(callback);
+    },
+  }, function(err, results) {
+    if(err) { next(err) }
+    if(results.genre === null) {
+      const err = new Error('Genre not found')
+      err.status = 404;
+      return next(err)
+    }
+    //Render if successfull
+    res.render('genre_detail', {title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books})
+  });
+
 };
 
 // Display Genre create form on GET.
--- models/genre.js
+++ models/genre.js
@@ -6,7 +6,7 @@ const GenreSchema = new Schema({
   name: { type: String, required: true, maxlength: 100, minlength: 3 },
 });
 
-GenreSchema.virtual("url").get(() => {
+GenreSchema.virtual("url").get(function() {
   return "/catalog/genre/" + this._id;
 });
 
