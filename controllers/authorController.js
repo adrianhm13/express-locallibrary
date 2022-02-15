426602643fd84bce8763e7cb9b127508f9318f26
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-15 17:45:26 +0100

    Fetching Authors and rendering view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,41 +1,50 @@
-const Author = require('../models/author')
+const Author = require("../models/author");
 
 // Display list of authors.
-exports.author_list = (req, res) => {
-    res.send ('NOT IMPLEMENTED: Author List')
-}
+exports.author_list = (req, res, next) => {
+  Author.find()
+    .sort([["family_name", "ascending"]])
+    .exec((err, list_authors) => {
+      if (err) return next(err);
+      // If successfull, then render
+      res.render("author_list", {
+        title: "Author List",
+        author_list: list_authors,
+      });
+    });
+};
 
 // Display detail page for specific author
 exports.author_detail = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id)
-}
+  res.send("NOT IMPLEMENTED: Author detail: " + req.params.id);
+};
 
 // Display author create form on GET
 exports.author_create_get = (req, res) => {
-    res.send ('NOT IMPLEMENTED: Author create GET')
-}
+  res.send("NOT IMPLEMENTED: Author create GET");
+};
 
 // Handle author create on POST
 exports.author_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author create POST')
-}
+  res.send("NOT IMPLEMENTED: Author create POST");
+};
 
 // Display Author delete form on GET.
 exports.author_delete_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author delete GET');
+  res.send("NOT IMPLEMENTED: Author delete GET");
 };
 
 // Handle Author delete on POST.
 exports.author_delete_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author delete POST');
+  res.send("NOT IMPLEMENTED: Author delete POST");
 };
 
 // Display Author update form on GET.
 exports.author_update_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author update GET');
+  res.send("NOT IMPLEMENTED: Author update GET");
 };
 
 // Handle Author update on POST.
 exports.author_update_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author update POST');
+  res.send("NOT IMPLEMENTED: Author update POST");
 };
--- models/author.js
+++ models/author.js
@@ -1,51 +1,61 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
-const AuthorSchema = new Schema(
-  {
-    first_name: {type: String, required: true, maxLength: 100},
-    family_name: {type: String, required: true, maxLength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
-  }
-);
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
 
 // Virtual for author's full name
-AuthorSchema
-.virtual('name')
-.get(function () {
-// To avoid errors in cases where an author does not have either a family name or first name
-// We want to make sure we handle the exception by returning an empty string for that case
-  let fullname = '';
+AuthorSchema.virtual("name").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let fullname = "";
   if (this.first_name && this.family_name) {
-    fullname = this.family_name + ', ' + this.first_name
+    fullname = this.family_name + ", " + this.first_name;
   }
   if (!this.first_name || !this.family_name) {
-    fullname = '';
+    fullname = "";
   }
   return fullname;
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {
-  let lifetime_string = '';
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
   if (this.date_of_birth) {
     lifetime_string = this.date_of_birth.getYear().toString();
   }
-  lifetime_string += ' - ';
+  lifetime_string += " - ";
   if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear()
+    lifetime_string += this.date_of_death.getYear();
   }
   return lifetime_string;
 });
 
 // Virtual for author's URL
-AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/' + this._id;
+AuthorSchema.virtual("url").get(function () {
+  return "/catalog/author/" + this._id;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  if (this.date_of_birth) {
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  if (this.date_of_death) {
+    return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
 });
 
 //Export model
-module.exports = mongoose.model('Author', AuthorSchema);
+module.exports = mongoose.model("Author", AuthorSchema);
