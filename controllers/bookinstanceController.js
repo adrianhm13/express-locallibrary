5f0fbc0ccc5c2effd37fd24635cc5735e3ef68bf
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-16 09:56:58 +0100

    Book instance detail view

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -16,8 +16,17 @@ exports.bookinstance_list = (req, res, next) => {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance detail: " + req.params.id);
+exports.bookinstance_detail = (req, res, next) => {
+  BookInstance.findById(req.params.id).populate('book').exec(function (err, bookinstance) {
+    if(err) {return next(err)}
+    if (bookinstance === null) {
+      const error = new Error('Book copy not found')
+      error.status = 404
+      return next(error)
+    }
+    // Successful, then render
+    res.render('bookinstance_detail', {title: 'Copy: ' + bookinstance.book.title, bookinstance})
+  })
 };
 
 // Display BookInstance create form on GET.
