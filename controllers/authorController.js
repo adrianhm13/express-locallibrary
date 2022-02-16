036c8a951612e4d18ed3674c9b455c791eeb0cd0
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-16 09:10:08 +0100

    Author detail view added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
 const Author = require("../models/author");
+const Book = require("../models/book")
+
+const async = require("async")
 
 // Display list of authors.
 exports.author_list = (req, res, next) => {
@@ -15,8 +18,25 @@ exports.author_list = (req, res, next) => {
 };
 
 // Display detail page for specific author
-exports.author_detail = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author detail: " + req.params.id);
+exports.author_detail = (req, res, next) => {
+  
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id).exec(callback)
+    },
+    authors_book: function(callback){
+      Book.find({'author': req.params.id }, 'title summary').exec(callback)
+    },
+  }, function (err, results) {
+    if(err){return next(err)}
+    if(results.author === null) {
+      const err = new Error('Author not found')
+      err.status = 404
+      return next(err)
+    }
+    //Successful, then render
+    res.render('author_detail', {title: 'Author Detail', author: results.author, author_books: results.authors_book})
+  })
 };
 
 // Display author create form on GET
